8b8ca46de3c10f311a9d303ad9fe85fc35c15238
John-MiracleWorker <58918509+John-MiracleWorker@users.noreply.github.com>
2026-07-23 05:41:14 -0400

    feat: export Adaptive Flock assignment service

--- src/nested_memvid_agent/routing/__init__.py
+++ src/nested_memvid_agent/routing/__init__.py
@@ -3,21 +3,26 @@ from .models import (
     AgentTaskContract,
     ModelTarget,
     PrivacyClass,
+    ProviderProfile,
     RouteCandidate,
     RouteDecision,
     RoutePolicy,
     RoutingMode,
 )
 from .router import ReviewDiversityContext, RoutingUnavailableError, route_task
+from .service import AdaptiveFlockRoutingService, RoutingAssignment
 
 __all__ = [
+    "AdaptiveFlockRoutingService",
     "AgentTaskContract",
     "ModelTarget",
     "PrivacyClass",
+    "ProviderProfile",
     "ReviewDiversityContext",
     "RouteCandidate",
     "RouteDecision",
     "RoutePolicy",
+    "RoutingAssignment",
     "RoutingMode",
     "RoutingUnavailableError",
     "TaskLike",
